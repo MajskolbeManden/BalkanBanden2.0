95ba74000e68ddd4f303124dd975de23f6a30293
DESKTOP-KHDRP1R\Frederik <ninjakongenfh@yahoo.com>
2017-12-21 11:04:20 +0100

    Notifications for windows Added. Requires new nuget package

--- SignalRServer/SignalRServer/SignalRServer.UWP/Notify.Win.cs
+++ SignalRServer/SignalRServer/SignalRServer.UWP/Notify.Win.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using SignalRServer.Models;
 using Xamarin.Forms;
 using Windows.ApplicationModel.Activation;
-//using Microsoft.Toolkit.Uwp.Notifications; //this is a xml to C# library, that helps creating notifications in C#
+using Microsoft.Toolkit.Uwp.Notifications; //this is a xml to C# library, that helps creating notifications in C#. NuGet Package.
 
 [assembly: Dependency(typeof(SignalRServer.UWP.Notify))]
 namespace SignalRServer.UWP
@@ -20,6 +20,34 @@ namespace SignalRServer.UWP
             //To be Added
             //Use Toast (Universal App notification system)
             //Toast constructor needs Visual but Action and Buttons can be added, but not required.
+            ToastVisual visual = new ToastVisual()
+            {
+                BindingGeneric = new ToastBindingGeneric()
+                {
+                    Children =
+                    {
+                        new AdaptiveText()
+                        {
+                        Text = "New Message"
+                        },
+
+                        new AdaptiveText()
+                        {
+                        Text = "From " + message.Sender
+                        }
+                    },
+                }
+            };
+
+            ToastContent toastContent = new ToastContent()
+            {
+                Visual = visual
+            };
+            // And create the toast notification
+            var toast = new ToastNotification(toastContent.GetXml());
+
+            //Send the notification
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
         }
     }
 }
