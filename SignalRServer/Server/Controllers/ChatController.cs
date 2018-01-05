1d5159447e57da3a52613c8beea656d4846440a8
Sameh Cheheiber <evilfridge@live.dk>
2018-01-05 14:19:52 +0100

    httpclient på web klient models og controllers

---
+++ SignalRServer/Server/Controllers/ChatController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using Server.Helper;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Server.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public async Task<ActionResult> Index()
+        {
+            var client = ChatHttpClient.GetClient();
+            HttpResponseMessage response = await client.GetAsync("api/SignalRMessage");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var model = JsonConvert.deserializeObject<IEnumerable<>>(content);
+
+                return View(model);
+            }
+            else
+                return Content("Der er sket en fejl...");
+                
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Chat/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Chat/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Chat/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Chat/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SignalRServer/SignalRServer/SignalRServer/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRServer.ViewModels
+{
+    public class LoginViewModel
+    {
+        public LoginViewModel()
+        {
+            
+        }
+    }
+}
